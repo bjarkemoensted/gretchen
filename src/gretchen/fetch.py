067b36d934b888b64245bb60a48f01da1f84910b
bjarkemoensted <bjarkemoensted@gmail.com>
2025-03-29 13:09:19 +0100

    ugh

--- src/gretchen/fetch.py
+++ src/gretchen/fetch.py
@@ -10,111 +10,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 from gretchen.caching import Cache
-from gretchen.sql import DBMixin
-
-
-class FuncNodeMixin(anytree.NodeMixin):
-    def disp(self, parent):
-        s = f"{self.name} - {parent.name}"
-        return s
-    
-    def _pre_detach(self, parent):
-        pass  #print(f"_pre_detach", self.disp(parent))
-    def _post_detach(self, parent):
-        pass  # print(f"_post_detach", self.disp(parent))
-    def _pre_attach(self, parent):
-        pass  # print(f"_pre_attach", self.disp(parent))
-    def _post_attach(self, parent):
-        pass  # print(f"_post_attach", self.disp(parent))
-
-
-def _infer_callable_name(f: Callable|None) -> str|None:
-    """Helper method for determining an appropriate representation of a callable"""
-    
-    # Try looking for name attribute
-    try:
-        return f.__name__
-    except AttributeError:
-        pass
-    
-    # Represent partial functions as function_name(arg1, arg2, k1=v1, k2=v2, ...)
-    if isinstance(f, functools.partial):
-        args_ = [str(arg) for arg in f.args]
-        kws_ = [f"{k}={v}" for k, v in f.keywords.items()]
-        argpart = ", ".join(args_ + kws_)
-            
-        res = f"{f.func.__name__}({argpart})"
-        return res
-    
-    return None
-
-
-class Fetch(FuncNodeMixin):
-    
-    def __init__(self, func: Callable=None, name: str=None, parent=None):
-        self.func = func
-        if name is None:
-            try:
-                name = func.__name__
-            except AttributeError:
-                pass
-        self.name = name
-        self.key = name
-        self.parent = parent
-
-    def add_function(self, func: Callable, name=None):
-        f = Fetch(func, name=name, parent=self)
-        return f
-
-
-    def __call__(self, *args, results_dict: dict=None, **kwargs):
-        if results_dict is None:
-            results_dict = dict()
-        
-        if self.func is None:
-            next_args = args
-            next_kwargs = kwargs
-        else:
-            res = self.func(*args, **kwargs)
-            
-            if self.key is not None:
-                assert self.key not in results_dict
-                results_dict[self.key] = res
-            next_args = (res,)
-            next_kwargs = dict()
-        
-        for child in self.children:
-            child(*next_args, results_dict=results_dict, **next_kwargs)
-        
-        return results_dict
-    
-    @property
-    def displayname(self):
-        name = self.name
-        if name is None:
-            name = _infer_callable_name(self.func)
-        if name is None and self.parent is None:
-            name = "root"
-        
-        return name
-            
-    def __repr__(self):
-        return f"<Node: {self.displayname}>"
-
-    def __str__(self):
-        lines = []
-        for pre, fill, node in anytree.RenderTree(self):
-            lines.append("%s%s" % (pre, node.displayname))
-        
-        s = "\n".join(lines)
-        return s
-    
-    def __getitem__(self, name: str):
-        for node in anytree.PreOrderIter(self):
-            if node.name == name:
-                return node
-            #
-        raise KeyError(f"No node with name {name} located under {repr(self)}")
 
 
 def parse_cp(html: str):
@@ -132,44 +27,25 @@ def read_url(url: str):
     r = requests.get(url)
     res = r.text
     return res
-
-
-
-def f(a, b=42):
-    print(f"Got {a=}, {b=}")
-    return a + b
-
-
-def g(x):
-    print(f"Got {x=}")
-    return 2*x
-
-
-class Scraper(DBMixin, Fetch):
-    def __init__(self, db_url: str, table_name_or_model: str|DeclarativeMeta, cache_kwargs: dict=None):
+    
+    
+class Scraper:
+    def __init__(self, cache_kwargs: dict=None):
         
         # url, table name, dict?
         if cache_kwargs is None:
             cache_kwargs = dict()
         
-        self.db_url = db_url
-        self.table = table_name_or_model
-        self._ensured_table_created = False
         self.cache = Cache(**cache_kwargs)
-        super().__init__()
-    
-    @property
-    def engine(self):
-        return self.get_engine(url=self.db_url)
-    
-    def _get_model(self):
-        pass  # !!!
+        self.scrapers = dict()
     
     def add_scraper(self, name: str, url: str, parser: Callable):
         cache_html = self.cache(read_url)
         read_and_cache = functools.partial(cache_html, url)
         
-        self.add_function(read_and_cache).add_function(parser, name=name)
+        f = lambda: parser(read_and_cache())
+        self.scrapers[name] = f
+        
         return self
 
 
